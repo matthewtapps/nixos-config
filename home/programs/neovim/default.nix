09b5f63e24ac548adf20d561514340ae4e5cc5ec
matthewtapps <mail@matthewtapps.com>
2024-11-10 12:16:21 +1100

    Delete comments, point nixfmt at nixfmt-rfc-style

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,40 +1,4 @@
-{ pkgs, ... }:
-# let
-#
-#   # treesitterWithGrammars = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-#   #   p.bash
-#   #   p.comment
-#   #   p.css
-#   #   p.dockerfile
-#   #   p.fish
-#   #   p.gitattributes
-#   #   p.gitignore
-#   #   p.go
-#   #   p.gomod
-#   #   p.gowork
-#   #   p.hcl
-#   #   p.javascript
-#   #   p.jq
-#   #   p.json5
-#   #   p.json
-#   #   p.lua
-#   #   p.make
-#   #   p.markdown
-#   #   p.nix
-#   #   p.python
-#   #   p.rust
-#   #   p.toml
-#   #   p.typescript
-#   #   p.vue
-#   #   p.yaml
-#   # ]));
-#   #
-#   # treesitter-parsers = pkgs.symlinkJoin {
-#   #   name = "treesitter-parsers";
-#   #   paths = treesitterWithGrammars.dependencies;
-#   # };
-# in
-{
+{ pkgs, ... }: {
   programs.neovim = {
     enable = true;
     package = pkgs.neovim;
@@ -62,7 +26,7 @@
       typescript-language-server
 
       # Formatters
-      nixfmt
+      nixfmt-rfc-style
       shfmt
       black
 
@@ -73,26 +37,10 @@
       vimPlugins.neotest-jest
     ];
 
-    # plugins = [ treesitterWithGrammars ];
   };
 
   home.file."./.config/nvim/" = {
     source = ./nvim;
     recursive = true;
   };
-
-  # home.file."./.config/nvim/lua/matt/init.lua".text = ''
-  #   require("matt.set")
-  #   require("matt.keymaps")
-  #   require("matt.lazy")
-  # '';
-  # vim.opt.runtimepath:append("${treesitter-parsers}")
-  # part of the above??
-
-  # Treesitter is configured as a locally developed module in lazy.nvim
-  # we hardcode a symlink here so that we can refer to it in our lazy config
-  # home.file."./.local/share/nvim/nix/nvim-treesitter/" = {
-  #   recursive = true;
-  #   source = treesitterWithGrammars;
-  # };
 }
