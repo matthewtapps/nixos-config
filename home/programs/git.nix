a9d2a334b346010e5b3435f14d98d6afd7cfadf3
matthewtapps <mail@matthewtapps.com>
2024-11-17 10:35:57 +1100

    Fix git config

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -7,12 +7,24 @@
     enable = true;
     userName = "matthewtapps";
     userEmail = "mail@matthewtapps.com";
-    ignores = [ ".direnv/" ".go/" ];
+    ignores = [
+      ".direnv/"
+      ".go/"
+    ];
     extraConfig = {
-      init = { defaultBranch = "main"; };
-      pull = { rebase = true; };
-      push = { autoSetupRemote = true; };
-      merge = { conflictstyle = "diff3"; };
+      init = {
+        defaultBranch = "main";
+      };
+      pull = {
+        rebase = true;
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      core.sshCommand = "ssh -i ~/.ssh/id_ed25519";
     };
   };
 }
