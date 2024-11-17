afe2b4963a5ef4db7b852edd388c9c24c281e7f1
Matthew Tapps <mail@matthewtapps.com>
2024-11-17 13:15:42 +1100

    Lan mouse configuration

--- home/programs/lan-mouse.nix
+++ home/programs/lan-mouse.nix
@@ -1,7 +1,7 @@
 { inputs, device, ... }:
 let
   config = {
-    "Matt-DESKTOP-NIXOS" = {
+    "desktop" = {
       right = {
         hostname = "NUC-NIXOS";
         ips = [ "192.168.0.181" ];
@@ -9,6 +9,14 @@ let
       left = {
         hostname = "Matt-THINKPAD";
         ips = [ "192.168.0.111" ];
+      };
+      systemd = true;
+    };
+
+    "nuc" = {
+      systemd = true;
+      authorized_fingerprints = {
+        "44:ba:df:4c:82:5b:93:84:38:9f:32:e4:e8:e8:09:0c:4e:97:4f:ac:29:42:93:38:14:c1:f5:34:da:22:88:3a" = "Matt-DESKTOP-NIXOS";
       };
     };
   };
