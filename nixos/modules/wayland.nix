f4b2a6d5924590b0ba55ad3fa66ffadca2be45cf
Matthew Tapps <mail@matthewtapps.com>
2024-11-30 09:18:40 +1100

    Better hyrpland and tuigreet config

--- nixos/modules/wayland.nix
+++ nixos/modules/wayland.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, inputs, ... }:
+{
   services = {
     xserver = {
       enable = true;
@@ -12,10 +13,10 @@
       enable = true;
       settings = {
         default_session = {
-          command =
-            "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd Hyprland";
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --asterisks --cmd Hyprland";
           user = "matt";
         };
+        terminal.vt = 2;
         Type = "idle";
         StandardInput = "tty";
         StandardOutput = "tty";
@@ -23,10 +24,25 @@
         TTYReset = true;
         TTYVHangup = true;
         TTYVTDisallocate = true;
-        After = [ "docker.service" "bluetooth.service" ];
+        After = [
+          "docker.service"
+          "bluetooth.service"
+        ];
       };
     };
   };
 
-  programs.hyprland.enable = true;
+  environment.sessionVariables = {
+    NIXOS_OZONE_LAYER = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
+
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage =
+      inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
 }
