ce921f78f184683f90132a89bb0a399db83e97cf
Matt <mail@matthewtapps.com>
2025-01-26 11:33:33 +1100

    Ags config customisations

--- home/programs/ags/widget/bar/TopBar.tsx
+++ home/programs/ags/widget/bar/TopBar.tsx
@@ -1,50 +1,48 @@
-import {App, Astal, Gtk} from "astal/gtk3"
+import { App, Astal, Gtk } from "astal/gtk3"
 import {
-    BatteryButton,
-    BluetoothButton,
-    ClockButton,
-    MenuButton,
-    MicrophoneButton,
-    NetworkButton,
-    ScreenRecordingButton,
-    VolumeButton, VpnButton,
-    Workspaces
+  BatteryButton,
+  BluetoothButton,
+  ClockButton,
+  MenuButton,
+  MicrophoneButton,
+  NetworkButton,
+  ScreenRecordingButton,
+  VolumeButton, VpnButton,
+  Workspaces
 } from "./BarWidgets";
 
-export default function () {
-    let iconCss = ""
+export default function() {
+  let iconCss = ""
 
-    return <window
-        css={`background: transparent;`}
-        monitor={0}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        margin={5}
-        anchor={Astal.WindowAnchor.TOP
-            | Astal.WindowAnchor.LEFT
-            | Astal.WindowAnchor.RIGHT}
-        application={App}>
-        <centerbox
-            className="window"
-            css={`
-                padding: 2px;
-                min-height: 40px;
+  return <window
+    css={`background: transparent;`}
+    monitor={0}
+    exclusivity={Astal.Exclusivity.EXCLUSIVE}
+    anchor={Astal.WindowAnchor.TOP
+      | Astal.WindowAnchor.LEFT
+      | Astal.WindowAnchor.RIGHT}
+    application={App}>
+    <centerbox
+      className="window"
+      css={`
+                min-height: 20px;
             `}>
-            <box halign={Gtk.Align.START}>
-                <MenuButton css={""}/>
-                <Workspaces vertical={false}/>
-            </box>
-            <box>
-                <ClockButton css={""} singleLine={true}/>
-            </box>
-            <box halign={Gtk.Align.END}>
-                <ScreenRecordingButton css={iconCss}/>
-                <VolumeButton css={iconCss}/>
-                <MicrophoneButton css={iconCss}/>
-                <BluetoothButton css={iconCss}/>
-                <VpnButton css={iconCss}/>
-                <NetworkButton css={iconCss}/>
-                <BatteryButton css={iconCss}/>
-            </box>
-        </centerbox>
-    </window>
+      <box halign={Gtk.Align.START}>
+        <MenuButton css={""} />
+        <Workspaces vertical={false} />
+      </box>
+      <box>
+        <ClockButton css={""} singleLine={true} />
+      </box>
+      <box halign={Gtk.Align.END}>
+        <ScreenRecordingButton css={iconCss} />
+        <VolumeButton css={iconCss} />
+        <MicrophoneButton css={iconCss} />
+        <BluetoothButton css={iconCss} />
+        <VpnButton css={iconCss} />
+        <NetworkButton css={iconCss} />
+        <BatteryButton css={iconCss} />
+      </box>
+    </centerbox>
+  </window>
 }
