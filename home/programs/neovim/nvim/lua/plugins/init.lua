61606720755892b9896504d9044a5270f18590fd
Matthew Tapps <mail@matthewtapps.com>
2025-01-28 23:35:58 +1100

    Neovim everforest config

--- home/programs/neovim/nvim/lua/plugins/init.lua
+++ home/programs/neovim/nvim/lua/plugins/init.lua
@@ -8,6 +8,19 @@ return {
       vim.cmd([[colorscheme everforest]])
       require("everforest").setup({
         background = "soft",
+        transparent_background_level = 0,
+        italics = true,
+        disable_italic_comments = true,
+        sign_column_background = "none",
+        ui_contrast = "low",
+        dim_inactive_windows = false,
+        diagnostic_text_highlight = true,
+        diagnostic_virtual_text = "coloured",
+        diagnostic_line_highlight = true,
+        spell_foreground = false,
+        inlay_hints_background = "dimmed",
+        show_eob = false,
+        float_style = "bright"
       })
     end,
   },
