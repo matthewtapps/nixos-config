4d39a44558736f39a8d73877ad000560cd5a539a
Matthew Tapps <mail@matthewtapps.com>
2025-01-28 23:36:01 +1100

    Wezterm config

--- home/programs/wezterm.nix
+++ home/programs/wezterm.nix
@@ -3,11 +3,19 @@ _: {
     enable = true;
     enableZshIntegration = true;
     extraConfig = ''
-      return {
-        color-scheme = "Everforest Dark Soft (Gogh)",
-        font = wezterm.font("GeistMono Nerd Font"),
-        enable_tab_bar = false,
+      local config = {}
+      config.color_scheme = "Everforest Dark Medium (Gogh)"
+      config.font = wezterm.font("GeistMono Nerd Font")
+      config.enable_tab_bar = false
+      config.window_padding = {
+        left = '0px',
+        right = '0px',
+        top = '0px',
+        bottom = '0.25cell'
       }
+      enable_scroll_bar = false
+
+      return config
     '';
   };
 }
