0b5cf477bee2cb4f6b49e6e1f524302bc584331f
Matthew Tapps <mail@matthewtapps.com>
2025-02-05 12:31:56 +1100

    Lan mouse changes

--- home/programs/lan-mouse.nix
+++ home/programs/lan-mouse.nix
@@ -5,28 +5,30 @@ let
       right = {
         hostname = "NUC-NIXOS";
         ips = [ "192.168.0.181" ];
+        activate_on_startup = true;
       };
       left = {
         hostname = "Matt-THINKPAD-NIXOS";
         ips = [ "192.168.0.111" ];
+        activate_on_startup = true;
       };
       systemd = true;
     };
 
     "nuc" = {
-      systemd = true;
       authorized_fingerprints = {
         "44:ba:df:4c:82:5b:93:84:38:9f:32:e4:e8:e8:09:0c:4e:97:4f:ac:29:42:93:38:14:c1:f5:34:da:22:88:3a" =
           "Matt-DESKTOP-NIXOS";
       };
+      systemd = true;
     };
 
     "thinkpad" = {
-      systemd = true;
       authorized_fingerprints = {
         "44:ba:df:4c:82:5b:93:84:38:9f:32:e4:e8:e8:09:0c:4e:97:4f:ac:29:42:93:38:14:c1:f5:34:da:22:88:3a" =
           "Matt-DESKTOP-NIXOS";
       };
+      systemd = true;
     };
   };
 in
