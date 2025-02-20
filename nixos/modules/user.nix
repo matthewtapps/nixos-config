19d815e109876533d2176596dee81c5f68948b47
matthewtapps <mail@matthewtapps.com>
2025-02-20 12:32:39 +1100

    Add anna user properly

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -1,8 +1,24 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   users.users.matt = {
     isNormalUser = true;
     description = "matt";
-    extraGroups = [ "wheel" "networkmanager" "video" ];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  users.users.anna = {
+    isNormalUser = true;
+    description = "anna";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "video"
+    ];
     shell = pkgs.zsh;
   };
 }
