45e7d9d5a3f822b135d2770118828231babb1360
Matt <mail@matthewtapps.com>
2025-04-08 11:58:38 +1000

    Tailscale module

--- nixos/modules/tailscale.nix
+++ nixos/modules/tailscale.nix
@@ -1,3 +1,19 @@
-_: {
+{ pkgs, ... }:
+{
   services.tailscale.enable = true;
+
+  services.tailscale.useRoutingFeatures = "both";
+
+  networking.firewall.checkReversePath = "loose";
+
+  environment = {
+    systemPackages = with pkgs; [
+      tailscale
+      ktailctl
+    ];
+  };
+
+  services.resolved = {
+    enable = true;
+  };
 }
