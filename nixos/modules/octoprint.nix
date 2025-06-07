cbfb13009c180dd2785592bff96bbd2ce594475d
Matthew Tapps <mail@matthewtapps.com>
2025-06-08 09:54:23 +1000

    Add nocaps to hyprland conf

--- nixos/modules/octoprint.nix
+++ nixos/modules/octoprint.nix
@@ -1,6 +1,23 @@
-_: {
+{ pkgs, ... }:
+{
   services.octoprint = {
     enable = true;
     user = "matt";
   };
+
+  environment.systemPackages = with pkgs; [
+    (orca-slicer.overrideAttrs (old: {
+      buildInputs = (old.buildInputs or [ ]) ++ [
+        intel-media-driver # Intel media/compute libraries
+        onetbb # Intel Threading Building Blocks (includes OpenMP)
+      ];
+      # Add Intel OpenMP to the runtime path
+      postInstall =
+        (old.postInstall or "")
+        + ''
+          wrapProgram $out/bin/orca-slicer \
+            --prefix LD_LIBRARY_PATH : ${pkgs.onetbb}/lib
+        '';
+    }))
+  ];
 }
