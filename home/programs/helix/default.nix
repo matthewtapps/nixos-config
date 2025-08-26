7c4a8e3def6414820ce1fac09b8bae65ee0b7e28
Matthew Tapps <mail@matthewtapps.com>
2025-08-26 10:26:32 +1000

    Update helix config

--- home/programs/helix/default.nix
+++ home/programs/helix/default.nix
@@ -1,9 +1,31 @@
 { pkgs, ... }:
+let
+  vim-hx = pkgs.callPackage ../../../nixos/packages/vim-hx.nix {};
+in
 {
   home.packages = with pkgs; [
-    evil-helix
-  ];
+    vim-hx
+    
+    # Language servers
+    nixd
+    lua-language-server
+    typescript-language-server
+    bash-language-server
+    astro-language-server
+    docker-language-server
+    gopls
+    terraform-ls
+    vscode-langservers-extracted
+    markdown-oxide
+    ruff
+    yaml-language-server
+    omnisharp-roslyn
+    tailwindcss-language-server
 
+    # Formatters
+    taplo
+  ];
+  
   home.file."./.config/helix/" = {
     source = ./helix;
     recursive = true;
