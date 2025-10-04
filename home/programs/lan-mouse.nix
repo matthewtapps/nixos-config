50262820d48e71821daaa85e52af2ea6e84a7db6
Matthew Tapps <mail@matthewtapps.com>
2025-10-04 16:22:40 +1000

    Lan-mouse settings

--- home/programs/lan-mouse.nix
+++ home/programs/lan-mouse.nix
@@ -2,17 +2,16 @@
 let
   config = {
     "desktop" = {
-      right = {
+      left = {
         hostname = "NUC-NIXOS";
         ips = [ "192.168.0.181" ];
         activate_on_startup = true;
       };
-      left = {
+      right = {
         hostname = "Matt-THINKPAD-NIXOS";
         ips = [ "192.168.0.111" ];
         activate_on_startup = true;
       };
-      systemd = true;
     };
 
     "nuc" = {
@@ -20,7 +19,6 @@ let
         "44:ba:df:4c:82:5b:93:84:38:9f:32:e4:e8:e8:09:0c:4e:97:4f:ac:29:42:93:38:14:c1:f5:34:da:22:88:3a" =
           "Matt-DESKTOP-NIXOS";
       };
-      systemd = true;
     };
 
     "thinkpad" = {
@@ -28,7 +26,6 @@ let
         "44:ba:df:4c:82:5b:93:84:38:9f:32:e4:e8:e8:09:0c:4e:97:4f:ac:29:42:93:38:14:c1:f5:34:da:22:88:3a" =
           "Matt-DESKTOP-NIXOS";
       };
-      systemd = true;
     };
   };
 in
