a20a22df081fe47ed5b747da871a32c156d1d456
Matthew Tapps <mail@matthewtapps.com>
2025-10-07 10:19:37 +1100

    Fix audio clock speed

--- nixos/modules/audio.nix
+++ nixos/modules/audio.nix
@@ -1,12 +1,18 @@
 { pkgs, ... }:
 {
-  services.pulseaudio.enable = false;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
+    extraConfig.pipewire = {
+      "99-min-quantum" = {
+        "context.properties" = {
+          "default.clock.min-quantum" = 1024;
+        };
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [ pulseaudio ];
