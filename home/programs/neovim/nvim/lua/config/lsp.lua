8a9c0b6c3fe041176f6fedb0115a3611b159eb75
Matthew Tapps <mail@matthewtapps.com>
2025-10-13 16:51:03 +1100

    Delete vim lsp keybinds in favour of snack pickers

--- home/programs/neovim/nvim/lua/config/lsp.lua
+++ home/programs/neovim/nvim/lua/config/lsp.lua
@@ -94,13 +94,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		end
 
 		-- Keymaps
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
 		vim.keymap.set("n", "<leader>cd", vim.lsp.buf.declaration, opts)
 	end,
 })
