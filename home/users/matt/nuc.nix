af70215aa3589a1a4a4c9904965b28f8a674be0c
matthewtapps <mail@matthewtapps.com>
2025-11-09 12:30:03 +1100

    Fix imports

--- home/users/matt/nuc.nix
+++ home/users/matt/nuc.nix
@@ -2,10 +2,11 @@ _: {
   imports = [
     ./common.nix
     ./theme.nix
-    ../../programs/ags/default.nix
     ../../programs/hypr/default.nix
     ../../programs/lan-mouse.nix
-    ../../programs/rofi/default.nix
     ../../programs/wezterm/default.nix
+    ../../programs/rofi.nix
+    ../../programs/stylix.nix
+    ../../programs/waybar/default.nix
   ];
 }
