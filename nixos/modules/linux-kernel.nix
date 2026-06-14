04c7042d1769b8bd2af10dce6638bb2ff388663a
matthewtapps <mail@matthewtapps.com>
2026-06-15 09:32:38 +1000

    Tidy up linux kernel module

--- nixos/modules/linux-kernel.nix
+++ nixos/modules/linux-kernel.nix
@@ -1,17 +1,5 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
-}:
-# let host = config.networking.hostName;
-# in lib.mkMerge [
-#   (lib.mkIf (host == "desktop") {
+{ pkgs, ... }:
+
 {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 }
-# })
-# (lib.mkIf (host != "desktop") {
-#   boot.kernelPackages = pkgs.linuxPackages_latest;
-# })
-# ]
